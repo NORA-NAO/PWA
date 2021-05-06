4f95bee0467168e43232dc483f35499591dc887d
NORA-NAO <eda.ynora@gmail.com>
2021-05-06 14:09:23 -0500

    PWA-responsive

--- sw.js
+++ sw.js
@@ -1,9 +1,53 @@
-self.addEventListener('install', function() {
-  console.log('Install!');
+const cacheName = "cym-v1";
+const staticAssets = [
+  './',
+  './index.html',
+  './Estadisticas.html',
+  './Principal.html',
+  './RegistrarA.html',
+  './RegistrarP.html', 
+  './style.css',
+  './style2.css',
+  './style3.css',
+  './style4.css',
+  './index.js'
+];
+
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
 });
-self.addEventListener("activate", event => {
-  console.log('Activate!');
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
 });
-self.addEventListener('fetch', function(event) {
-  console.log('Fetch!', event.request);
+
+self.addEventListener('fetch', async e => {
+  const req = e.request;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
 });
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
